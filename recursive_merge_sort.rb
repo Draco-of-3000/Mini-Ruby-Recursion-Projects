b53ad0e8ce34e81a4d1cf118be5f4c8c4ea37a79
Draco <kevin.tenisson@gmail.com>
2023-07-05 04:38:08 -0700

    Added a function to merge array

--- recursive_merge_sort.rb
+++ recursive_merge_sort.rb
@@ -1,3 +1,6 @@
+numbers = [9,8,1,2,4,6,7,5,0,3]
+
+
 def merge_sort(arr)
     return arr if arr.length <= 1
 
@@ -7,3 +10,19 @@ def merge_sort(arr)
 
     merge(left, right)
 end
+
+def merge(left, right)
+    sorted = []
+
+    while !left.empty? && !right.empty?
+        if left[0] <= right[0]
+            sorted << left.shift
+        else 
+            sorted << right.shift
+        end
+    end
+
+    sorted.concat(left).concat(right)
+end
+
+puts merge_sort(numbers)
